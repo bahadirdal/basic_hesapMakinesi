a73ded1e9a50f093ee6c7de4750c34eb1591c1ba
Mustafa Bahadır Dal <77547385+bahadirdal@users.noreply.github.com>
2023-01-16 19:30:16 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+import 'anaekran.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Hesap Makinesi",
+      home: Iskele(),
+    );
+  }
+}
+
+class Iskele extends StatelessWidget {
+  @override
+Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Basic Hesap Makinesi"),),
+      body: AnaEkran(),
+    );
+  }
+}
+
+
+
